94f765241d661ced9cdc044531a1a049e28a8db5
Resam Taghipour <resam.t@gmail.com>
2023-06-21 17:22:31 +0330

    Colors

--- lib/data/resources/app_colors.dart
+++ lib/data/resources/app_colors.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  ///App
-  static Color get appBackground => _white;
 
-  ///AppBar
-  static Color get appBarBackground => _persianGreen;
-  static Color get appBarText => _white;
-
-  ///Bottom Navigation Bar
-  static Color get bottomBarBackground => _persianGreen;
-  static Color get bottomBarCircleBackground => _white;
-  static Color get bottomBarSelected => _white;
-  static Color get bottomBarUnselected => _white;
-  static Color get bottomBarIcon => _persianGreen;
-  static Color get bottomBarText => _white;
+  ///Basics
+  static Color get transparent => Colors.transparent;
+  static Color get appBackground => _pinkSalmon;
 
   ///Colors
   static Color get _white => Colors.white;
   static Color get _black => Colors.black;
-  static Color get _persianGreen => const Color(0xff00a693);
+  static Color get _pinkSalmon => const Color(0xffff9999);
+
+  ///Generals
+  //Texts
+  static Color get _textNormal => _black;
+  static Color get _textNormalWhite => _white;
+
+  ///Modules
+  //AppBar
+  // static Color get appBarBackground => _persianGreen;
+  static Color get appBarBackground => appBackground.withOpacity(0.6);
+  static Color get appBarText => _textNormalWhite;
+
+  //Bottom Navigation Bar
+  static Color get bottomBarBackground => appBarBackground.withOpacity(0.6);
+  static Color get bottomBarCircleBackground => bottomBarBackground;
+  static Color get bottomBarSelected => _textNormalWhite;
+  static Color get bottomBarUnselected => _textNormal;
+  static Color get bottomBarIcon => _textNormalWhite;
+  static Color get bottomBarText => _textNormalWhite;
+
 }
