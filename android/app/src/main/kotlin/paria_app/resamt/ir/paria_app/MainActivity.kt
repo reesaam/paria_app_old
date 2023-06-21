8034cedfa9574389d7b918b335bfd48732ba3db9
Resam Taghipour <resam.t@gmail.com>
2023-06-21 14:48:59 +0330

    Project Creation

---
+++ android/app/src/main/kotlin/paria_app/resamt/ir/paria_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package paria_app.resamt.ir.paria_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/pariaapp/resamt/ir/paria_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package pariaapp.resamt.ir.paria_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
