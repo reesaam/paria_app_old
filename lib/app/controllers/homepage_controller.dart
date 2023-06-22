4ae5845d3e8eff40a36501eb97953d1d4a1aa647
Resam Taghipour <resam.t@gmail.com>
2023-06-22 19:13:07 +0330

    HomePage Controller

--- lib/app/controllers/homepage_controller.dart
+++ lib/app/controllers/homepage_controller.dart
@@ -1,7 +1,11 @@
+import 'package:get/get.dart';
 import 'package:paria_app/core/elements/core_controller.dart';
 import 'package:paria_app/data/resources/app_page_details.dart';
 
 class HomePageController extends CoreController {
+
+  late RxString time;
+  late RxString date;
 
   @override
   void dataInit() {}
@@ -9,6 +13,9 @@ class HomePageController extends CoreController {
   @override
   void pageInit() {
     pageDetail = AppPageDetails.homepage;
+    DateTime dt = DateTime.now();
+    time = '${dt.year}/${dt.month}/${dt.day}'.obs;
+    date = '${dt.hour}:${dt.minute}:${dt.second}'.obs;
   }
 
   @override
