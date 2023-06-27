4e040b394ae9b6ee8818811c62af78bb9f50cd03
Resam Taghipour <resam.t@gmail.com>
2023-06-27 18:29:12 +0330

    Local Storage

--- lib/data/storage/local_storage.dart
+++ lib/data/storage/local_storage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:paria_app/data/data_models/accounts_data_models/account_records/account_records.dart';
+import 'package:paria_app/data/data_models/core_data_models/app_contact/app_contact.dart';
 
 class AppLocalStorage {
   String keyLocalStorage = 'Local Storage';
@@ -14,12 +15,26 @@ class AppLocalStorage {
   // dynamic emptyList(T) => List<T>.empty(growable: true);
 
   ///Keys
+  static const keyContacts = 'CONTACTS';
   static const keyAccountsRecords = 'ACCOUNTS_RECORDS';
 
   void clearStorage() {
     _storage.remove(keyAccountsRecords);
   }
 
+  ///Contacts
+  Future<void> saveContactsRecords(List<AppContact> listContacts) async =>
+      await _storage.write(keyContacts, listContacts);
+
+  List<AppContact>? loadContactsRecords() {
+    dynamic data = _storage.read(keyContacts);
+    return data == null
+        ? List.empty(growable: true)
+        : List.generate(
+            data.length, (index) => AppContact.fromJson(data[index]));
+  }
+
+  ///Accounts
   Future<void> saveAccountsRecords(List<AccountRecord> listRecords) async =>
       await _storage.write(keyAccountsRecords, listRecords);
 
