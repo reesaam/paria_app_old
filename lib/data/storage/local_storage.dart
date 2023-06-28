a16bac13175c90ac62fb478cff73815903f5c4d3
Resam Taghipour <resam.t@gmail.com>
2023-06-28 21:09:03 +0330

    Local Storage

--- lib/data/storage/local_storage.dart
+++ lib/data/storage/local_storage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:paria_app/data/data_models/accounts_data_models/account_records/account_records.dart';
+import 'package:paria_app/data/data_models/accounts_data_models/account_records/account_record.dart';
 import 'package:paria_app/data/data_models/core_data_models/app_contact/app_contact.dart';
 
 class AppLocalStorage {
@@ -24,26 +24,24 @@ class AppLocalStorage {
   }
 
   ///Contacts
-  Future<void> saveContactsRecords(List<AppContact> listContacts) async =>
+  Future<void> saveContactsRecords(AppContactsList listContacts) async =>
       await _storage.write(_keyContacts, listContacts);
 
-  List<AppContact>? loadContactsRecords() {
-    dynamic data = _storage.read(_keyContacts);
+  AppContactsList loadContactsRecords() {
+    final data = _storage.read<Map<String, dynamic>>(_keyContacts);
     return data == null
-        ? List.empty(growable: true)
-        : List.generate(
-            data.length, (index) => AppContact.fromJson(data[index]));
+        ? AppContactsList()
+        : AppContactsList.fromJson(data);
   }
 
   ///Accounts
-  Future<void> saveAccountsRecords(List<AccountRecord> listRecords) async =>
+  Future<void> saveAccountsRecords(AccountRecordsList listRecords) async =>
       await _storage.write(_keyAccountsRecords, listRecords);
 
-  List<AccountRecord>? loadAccountsRecords() {
-    dynamic data = _storage.read(_keyAccountsRecords);
+  AccountRecordsList loadAccountsRecords() {
+    final data = _storage.read<Map<String, dynamic>>(_keyAccountsRecords);
     return data == null
-        ? List.empty(growable: true)
-        : List.generate(
-            data.length, (index) => AccountRecord.fromJson(data[index]));
+        ? AccountRecordsList()
+        : AccountRecordsList.fromJson(data);
   }
 }
