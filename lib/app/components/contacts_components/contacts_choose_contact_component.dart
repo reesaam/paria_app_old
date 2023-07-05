f28992815d5e89212ee58f6a32e61e6bd825dd72
Resam Taghipour <resam.t@gmail.com>
2023-07-05 22:17:09 +0330

    ChooseContact

---
+++ lib/app/components/contacts_components/contacts_choose_contact_component.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:paria_app/app/components/app_general_components/app_dialogs.dart';
+import 'package:paria_app/data/data_models/core_data_models/app_contact/app_contact.dart';
+import 'package:paria_app/data/resources/app_paddings.dart';
+import 'package:paria_app/data/resources/app_text_styles.dart';
+import 'package:paria_app/data/resources/app_texts.dart';
+import 'package:paria_app/data/storage/local_storage.dart';
+
+class ChooseContactComponent {
+  final AppContactsList _listContacts =
+      AppLocalStorage.to.loadContactsRecords();
+  AppContact? _selectedContact;
+
+  Widget _chooseContactItem(AppContact contact) => GestureDetector(
+      onTap: () => {_selectedContact = contact, Get.back()},
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: AppPaddings.accountsSelectContactList,
+        child: Text('${contact.firstName} ${contact.lastName}',
+            style: AppTextStyles.contactsChooseContact),
+      ));
+
+  Widget _chooseContactForm() => Form(
+        child: Column(
+            children: List.generate(
+                _listContacts.contactsList.length,
+                (index) =>
+                    _chooseContactItem(_listContacts.contactsList[index]))),
+      );
+
+  Future<AppContact?> chooseContact() async {
+    await AppDialogs.mainAppDialogWithCancel(
+        AppTexts.accountsAddNewRecordChooseContact, _chooseContactForm());
+    return _selectedContact;
+  }
+}
