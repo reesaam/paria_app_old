365bd00ceada9c4014d97b782e671abba4153dde
Resam Taghipour <resam.t@gmail.com>
2023-07-18 22:02:59 +0330

    AppBar leading and action icons

--- lib/app/components/app_bar/app_bar.dart
+++ lib/app/components/app_bar/app_bar.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:paria_app/data/data_models/core_data_models/app_page_detail/app_page_detail.dart';
 import 'package:paria_app/data/resources/app_colors.dart';
+import 'package:paria_app/data/resources/app_paddings.dart';
 import 'package:paria_app/data/resources/app_text_styles.dart';
 
 class AppAppBar extends AppBar {
-  AppAppBar({super.key, required this.pageDetail}) : super();
+  AppAppBar(
+      {super.key, required this.pageDetail, this.barLeading, this.barAction})
+      : super();
 
   final AppPageDetail pageDetail;
+  final Widget? barLeading;
+  final Widget? barAction;
 
   @override
   Widget? get title => Text(pageDetail.pageName ?? '', style: AppTextStyles.appBarTitle);
+
+  @override
+  Widget? get leading => barLeading;
+
+  @override
+  List<Widget>? get actions => [Padding(padding: AppPaddings.appBarActions, child: barAction)];
 
   @override
   Color? get backgroundColor => AppColors.appBarBackground;
