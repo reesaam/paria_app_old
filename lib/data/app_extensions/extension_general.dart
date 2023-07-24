d4795d8d8b15e28253b9038a783a24adf6977c7d
Resam Taghipour <resam.t@gmail.com>
2023-07-24 20:09:45 +0330

    Icon Extension

--- lib/data/app_extensions/extension_general.dart
+++ lib/data/app_extensions/extension_general.dart
@@ -5,5 +5,8 @@ import 'package:paria_app/data/resources/app_icons.dart';
 extension IconColor on Icon {
   Icon get withAppDefaultColor => Icon(icon, color: AppColors.appDefaultColor);
   Icon get withAppAppBackgroundColor => Icon(icon, color: AppColors.appBackground);
+}
+
+extension IconSize on Icon {
   Icon withSize(double size) => Icon(icon, size: size);
 }
