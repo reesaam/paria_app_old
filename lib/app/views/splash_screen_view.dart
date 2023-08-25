2f2060826141396fd266eac1a9cc7be5ce274d38
Resam Taghipour <resam.t@gmail.com>
2023-08-25 11:43:00 +0330

    SplashScreen View

--- lib/app/views/splash_screen_view.dart
+++ lib/app/views/splash_screen_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:paria_app/app/controllers/splash_screen_controller.dart';
 import 'package:paria_app/core/elements/core_view.dart';
+import 'package:paria_app/data/resources/app_paddings.dart';
+import 'package:paria_app/data/resources/app_progress_indicator.dart';
 import 'package:paria_app/data/resources/app_text_styles.dart';
 
 class SplashScreenPage extends CoreView<SplashScreenController> {
@@ -27,7 +29,12 @@ class SplashScreenPage extends CoreView<SplashScreenController> {
         alignment: Alignment.center,
         width: Get.width,
         height: Get.height - 100,
-        child: Stack(children: [widgetLogo(), widgetAppName(), widgetVersion()]),
+        child: Stack(children: [
+          widgetLogo(),
+          widgetAppName(),
+          widgetLoadingIndicator(),
+          widgetVersion(),
+        ]),
       );
 
   Widget widgetLogo() => Container(
@@ -40,6 +47,11 @@ class SplashScreenPage extends CoreView<SplashScreenController> {
         controller.appName,
         style: AppTextStyles.splashScreenAppName,
       ));
+
+  Widget widgetLoadingIndicator() => Container(
+      alignment: Alignment.center,
+      padding: AppPaddings.splashScreenProgressIndicator,
+      child: AppProgressIndicator.circularDefault());
 
   Widget widgetVersion() => Container(
       alignment: Alignment.bottomCenter, child: Text(controller.appVersion));
