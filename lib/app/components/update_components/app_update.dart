4a9de05cf5b39fe6dba163fdcc5d54807dc823eb
Resam Taghipour <resam.t@gmail.com>
2023-11-14 16:04:32 +0330

    Dio Implementation

--- lib/app/components/update_components/app_update.dart
+++ lib/app/components/update_components/app_update.dart
@@ -1,17 +1,42 @@
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
+import 'package:paria_app/data/remote/dio/core_dio.dart';
 
 import '../../../core/admin/app_core_functions.dart';
 import '../../../data/resources/app_texts.dart';
 import '../../../data/resources/app_urls.dart';
 
 class AppCheckUpdate {
-  Future<String> checkVersion() async {
+  Future<String> checkVersion() async => _checkVersionDio();
+
+  ///HTTP
+  Future<String> _checkVersionHttp() async {
     String version = AppTexts.generalNotAvailableInitials;
     final response = await http.get(Uri.parse(AppURLs.appUrlResamHostVersion));
     appDebugPrint('Data HTTP Get:');
     appDebugPrint('Data body: ${response.body}');
     appDebugPrint('Status Code: ${response.statusCode}');
     response.statusCode == 200 ? version = response.body : null;
+    return version;
+  }
+
+  ///Dio
+  Future<String> _checkVersionDio() async {
+    String version = AppTexts.generalNotAvailableInitials;
+    Dio dio = DioCore().instance;
+
+    try {
+      Response response = await dio.get(AppURLs.appUrlResamHostVersion,
+          options: DioCore().dioOptions);
+      appDebugPrint('StatusCode: ${response.statusCode}');
+      appDebugPrint('StatusMessage: ${response.statusMessage}');
+      response.statusCode == 200 ? version = response.data : null;
+    } on DioException catch (dioEx) {
+      throw dioEx;
+    } catch (ex) {
+      throw ex;
+    }
+
     return version;
   }
 }
---
+++ lib/data/remote/dio/core_dio.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:paria_app/data/resources/app_urls.dart';
+
+class DioCore {
+  Duration get _defaultTimeOut => const Duration(seconds: 10);
+  Duration get _defaultConnectTimeOut => _defaultTimeOut;
+  Duration get _defaultSendTimeOut => _defaultTimeOut;
+  Duration get _defaultReceiveTimeOut => _defaultTimeOut;
+
+  BaseOptions get _baseOptions => BaseOptions(
+      baseUrl: AppURLs.baseHostUrl,
+      sendTimeout: _defaultSendTimeOut,
+      receiveTimeout: _defaultReceiveTimeOut,
+      connectTimeout: _defaultConnectTimeOut);
+
+  Dio get instance => Dio(_baseOptions);
+
+  Options get dioOptions => Options(
+      receiveTimeout: _defaultReceiveTimeOut, sendTimeout: _defaultSendTimeOut);
+}
